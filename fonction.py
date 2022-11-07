75e3642d9410f216f88916cb36eff172bc0d55a3
Urukagall <dany.jorge@free.fr>
2022-11-07 11:31:59 +0100

    truc

---
+++ fonction.py
@@ -0,0 +1,22 @@
+def add(x , y):
+    return (x + y)
+
+def sub(x , y):
+    return(x - y)
+
+def multiply(x , y):
+    return(x * y)
+
+def divide(x , y):
+    return(x / y)
+
+def modulo(x , y):
+    return(x % y)
+
+def salaires(salaireh , hjo , joan):
+    return((salaireh*hjo*joan)/365/24/60/60)
+
+print(salaires(10,8,300))
+
+def salairenet(net):
+    return(net*1.25)
