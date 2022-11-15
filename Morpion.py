c5121ef123ff54a9df2aba0745371d5caf01dc37
Urukagall <dany.jorge@free.fr>
2022-11-15 13:43:44 +0100

    gtech > gart

--- Morpion.py
+++ Morpion.py
@@ -10,14 +10,16 @@ def VerifAlign(tableau):
             return 1, 0,True
         elif tableau[0][x] == "O" and tableau[1][x] == "O" and tableau[2][x] == "O" :
             return 0, 1,True
-        if tableau[0][0] == "X" and tableau[1][1] == "X" and tableau[2][2] == "X" :
-            return 1, 0,True
-        elif tableau[0][0] == "O" and tableau[1][1] == "O" and tableau[2][2] == "O" :
-            return 0, 1,True
-        if tableau[0][2] == "X" and tableau[1][1] == "X" and tableau[2][0] == "X" :
-            return 1, 0,True
-        elif tableau[0][2] == "O" and tableau[1][1] == "O" and tableau[2][0] == "O" :
-            return 0, 1,True
+    if tableau[0][0] == "X" and tableau[1][1] == "X" and tableau[2][2] == "X" :
+        return 1, 0,True
+    elif tableau[0][0] == "O" and tableau[1][1] == "O" and tableau[2][2] == "O" :
+        return 0, 1,True
+    if tableau[0][2] == "X" and tableau[1][1] == "X" and tableau[2][0] == "X" :
+        return 1, 0,True
+    elif tableau[0][2] == "O" and tableau[1][1] == "O" and tableau[2][0] == "O" :
+        return 0, 1,True
+    if tableau[0].count(" ") == 0 and tableau[1].count(" ") == 0 and tableau[2].count(" ") == 0  :
+        return 0, 0,True
     return 0,0,False
 
 def Morpion():
@@ -30,18 +32,20 @@ def Morpion():
     while nbPuntosEnnemie < 3 and nbPuntosPlayer < 3 :
         while not(gameFinish):
             print("",tableau[0],"\n",tableau[1],"\n",tableau[2])
-            choicePlayer[0] = input("Met ta coordonées x\n")
-            choicePlayer[1] = input("Met ta coordonées y\n")
+            choicePlayer[0] = input("Met ta coordonées y\n")
+            choicePlayer[1] = input("Met ta coordonées x\n")
             while tableau[int(choicePlayer[0])][int(choicePlayer[1])] != " ":
-                choicePlayer[0] = input("Met ta coordonées x\n")
-                choicePlayer[1] = input("Met ta coordonées y\n")
+                choicePlayer[0] = input("Met ta coordonées y\n")
+                choicePlayer[1] = input("Met ta coordonées x\n")
             tableau[int(choicePlayer[0])][int(choicePlayer[1])] = "O"
-            choiceEnnemie[0] = random.randint(0,2)
-            choiceEnnemie[1] = random.randint(0,2)
-            while tableau[int(choiceEnnemie[0])][int(choiceEnnemie[1])] != " ":
+            liste = VerifAlign(tableau)
+            if liste[2] == False :
                 choiceEnnemie[0] = random.randint(0,2)
                 choiceEnnemie[1] = random.randint(0,2)
-            tableau[int(choiceEnnemie[0])][int(choiceEnnemie[1])] = "X"
+                while tableau[int(choiceEnnemie[0])][int(choiceEnnemie[1])] != " ":
+                    choiceEnnemie[0] = random.randint(0,2)
+                    choiceEnnemie[1] = random.randint(0,2)
+                tableau[int(choiceEnnemie[0])][int(choiceEnnemie[1])] = "X"
             liste = VerifAlign(tableau)
             nbPuntosEnnemie += liste[0]
             nbPuntosPlayer += liste[1]
@@ -51,8 +55,14 @@ def Morpion():
             print("L'ennemie a gagner avec ", nbPuntosEnnemie , " puntos contre ", nbPuntosPlayer , " puntos pour le joueur")
         elif liste[1] == 1 :
             print("Le joueur a gagner avec ", nbPuntosPlayer , " puntos contre ", nbPuntosEnnemie , " puntos pour l'ennemie")
+        else :
+            print("égalter")
         tableau=[[" "," "," "],[" "," "," "],[" "," "," "]]
         gameFinish = False
+    if nbPuntosEnnemie == 3 :
+        print("L'ennemie gane")
+    elif nbPuntosPlayer == 3 :
+        print("Le joueur gane")
 
 
 
--- PFC.py
+++ PFC.py
@@ -5,23 +5,38 @@ import random
 
 #Début
 
-#définir la fonction PFC
-    #initialise playerWin en booléen et sur False
-    #initialise iaWin en booléen et sur False
-    #initialise la variable ia avec la valeur retourner par l'éxecution de la fonction random qui prendra en argument x=0 et y=2
-    #initialise la variable joueur avec le retour de l'éxecution de la fonction input
-    #si la variable joueur est égale à 0,1 ou 2 qui correspond dans l'ordre a pierre, feuille et ciseaux
+#definir la fonction convertPFC qui prend en argument le choix
+    #si le choix est égale a 0
+        #alors retourne "pierre"
+    # ou sinon le choix est égale a 1
+        #alors retourne "feuille"
+    #ou sinon le choix est égale a 2
+        #alors retourne "ciseaux"
+
+#définir la fonction PFC aui prend en argument le nombre de puntos pour gagner
+    #initialise la variable nbPuntosPlayer a 0
+    #initialise la variable nbPuntosEnnemie a 0
+    #tant que nbPuntosPlayer est inférieur a nbPuntos et nbPuntosEnnemie est inférieur a nbPuntos
+        #initialise la variable ennemie avec la valeur retourner par l'éxecution de la fonction random qui prendra en argument x=0 et y=2
+        #initialise la variable player avec le retour de l'éxecution de la fonction input
+        #tant que player n'est pas égale a "0" "1" ou "2"
+            #alors affiche "dommage réessaie" 
+            #la variable player est égale au retour de l'éxecution de la fonction input
         #alors la variable joueur est convertit en integer
-        #si la variable ennemie est égale a la variable
-            #alors retourne "egalter"
+        #si la variable ennemie est égale a celle de joueur
+            #alors affiche "égalter" et le nombre de puntos de chacun
         #ou sinon la variable ennemie est égale a la variable joueur plus 1 (l'ennemie gagne avec un ciseaux ou une feuille)
-            #alors retourne "l'ennemie gane"
+            #alors affiche "l'ennemie gane" et le nombre de puntos de chacun
         #ou sinon la variable joueur est égale a la variable ennemie plus 1 (le joueur gagne avec un ciseaux ou une feuille)
-            #alors retourne "le joueur gane"
+            #alors affiche "le joueur gane" et le nombre de puntos de chacun
         #ou sinon la variable ennemie est égale à 0 et la variable joueur est égale à 2 (l'ennemie gagne avec une pierre)
-            #alors retourne "l'ennemie gane"
+            #alors affiche "l'ennemie gane" et le nombre de puntos de chacun
         #ou sinon la variable joueur est égale à 0 et la variable ennemie est égale à 2 (le joueur gagne avec une pierre)
-            #alors retourne "le joueur gane"
+            #alors affiche "le joueur gane" et le nombre de puntos de chacun
+    #si la variable nbPuntosEnnemie est égale a 3
+        #alors affiche "l'ennemie gane"
+    #ou sinon la variable nbPuntosPlayer est égale a 3
+        #alors affiche "le joueur gane"
 
 #Fin
 
@@ -36,32 +51,32 @@ def convertPFC(choice):
         return "ciseaux"
 
 def PFC(nbPuntos):
-    nbPuntosJoueur=0
+    nbPuntosPlayer=0
     nbPuntosEnnemie=0
-    while nbPuntosJoueur < nbPuntos and nbPuntosEnnemie < nbPuntos :
+    while nbPuntosPlayer < nbPuntos and nbPuntosEnnemie < nbPuntos :
         ennemie = random.randint(0,2)
-        joueur = input("0- pierre\n1- feuille\n2- ciseaux\n")
-        while joueur != "0" and  joueur != "1" and  joueur != "2" :
+        player = input("0- pierre\n1- feuille\n2- ciseaux\n")
+        while player != "0" and  player != "1" and  player != "2" :
             print("dommage réessaie")
-            joueur = input("0- Pierre\n1- feuille\n3- ciseaux\n")
-        joueur = int(joueur)
-        if ennemie == joueur :
-            print("égalter" + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosJoueur) + " Puntos")
-        elif ennemie == joueur + 1 :
+            player = input("0- Pierre\n1- feuille\n3- ciseaux\n")
+        player = int(player)
+        if ennemie == player :
+            print("égalter" + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosPlayer) + " Puntos")
+        elif ennemie == player + 1 :
             nbPuntosEnnemie += 1
-            print("l'ennemie gane avec " + convertPFC(ennemie) + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosJoueur) + " Puntos")
-        elif joueur == ennemie + 1 :
-            nbPuntosJoueur += 1
-            print("le joueur gane avec " + convertPFC(joueur) + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosJoueur) + " Puntos")
-        elif ennemie == 0 and joueur == 2 :
+            print("l'ennemie gane avec " + convertPFC(ennemie) + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosPlayer) + " Puntos")
+        elif player == ennemie + 1 :
+            nbPuntosPlayer += 1
+            print("le joueur gane avec " + convertPFC(player) + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosPlayer) + " Puntos")
+        elif ennemie == 0 and player == 2 :
             nbPuntosEnnemie += 1
-            print("l'ennemie gane avec " + convertPFC(ennemie) + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosJoueur) + " Puntos")
-        elif ennemie == 2 and joueur == 0 :
-            nbPuntosJoueur += 1
-            print("le joueur gane avec " + convertPFC(joueur) + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosJoueur) + " Puntos")
+            print("l'ennemie gane avec " + convertPFC(ennemie) + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosPlayer) + " Puntos")
+        elif ennemie == 2 and player == 0 :
+            nbPuntosPlayer += 1
+            print("le joueur gane avec " + convertPFC(player) + " l'ennemie a " + str(nbPuntosEnnemie) + " Puntos" + " le joueur a " + str(nbPuntosPlayer) + " Puntos")
     if nbPuntosEnnemie == 3:
-        return "l'ennemie gane"
-    elif nbPuntosJoueur == 3:
-        return "le joueur gane"
+        print("l'ennemie gane")
+    elif nbPuntosPlayer == 3:
+        print("le joueur gane")
 
-print(PFC(3))
+PFC(3)
